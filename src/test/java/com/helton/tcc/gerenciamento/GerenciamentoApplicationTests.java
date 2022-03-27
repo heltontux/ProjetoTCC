5cc98ffc8811be958a24848bc34c69b899ad361a
Helton Augusto <heltontux@gmail.com>
2022-03-27 17:09:41 -0300

    Inicio do projeto

---
+++ src/test/java/com/helton/tcc/gerenciamento/GerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helton.tcc.gerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
