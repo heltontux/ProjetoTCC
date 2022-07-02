84e33c5b7e8d899366038a4ddbcda180f0056528
Helton Augusto <heltontux@gmail.com>
2022-07-02 13:56:34 -0300

    alterações na classe principal e pom.xml para deploy no tomcat

--- src/main/java/com/helton/tcc/gerenciamento/GerenciamentoApplication.java
+++ src/main/java/com/helton/tcc/gerenciamento/GerenciamentoApplication.java
@@ -2,9 +2,10 @@ package com.helton.tcc.gerenciamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GerenciamentoApplication {
+public class GerenciamentoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciamentoApplication.class, args);
